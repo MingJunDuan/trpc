7c29c78bbb4a2e797d9a28bb20e029a1e8232a12
MingJunDuan <dmj1161859184@126.com>
2021-02-08 09:19:04 +0800

    Add volatile

--- trpc-client/src/main/java/com/netty/trpc/client/connect/ConnectionManagerFactory.java
+++ trpc-client/src/main/java/com/netty/trpc/client/connect/ConnectionManagerFactory.java
@@ -6,14 +6,34 @@ package com.netty.trpc.client.connect;
  * @date 2021-04-15 22:26
  */
 public class ConnectionManagerFactory {
-    private static final ConnectionManager instance = new ConnectionManager();
-    private static final ForkingConnectionManager forkingInstance = new ForkingConnectionManager();
+    //volatile is necessary
+    private static volatile ConnectionManager instance;
+    private static volatile ForkingConnectionManager forkingInstance;
 
+    /**
+     * Lazy initialization
+     *
+     * @return
+     */
     public static ConnectionManager getConnectionManager(){
+        if (instance==null){
+            synchronized (ConnectionManagerFactory.class){
+                if (instance==null){
+                    instance = new ConnectionManager();
+                }
+            }
+        }
         return instance;
     }
 
     public static ForkingConnectionManager getForkingConnectionManager(){
+        if (forkingInstance==null){
+            synchronized (ConnectionManagerFactory.class){
+                if (forkingInstance==null){
+                    forkingInstance = new ForkingConnectionManager();
+                }
+            }
+        }
         return forkingInstance;
     }
 }
