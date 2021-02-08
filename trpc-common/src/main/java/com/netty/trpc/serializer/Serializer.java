0f8dd55a2d582cc9f101461b3e41f70b70de1857
duanmingjun <dmj1161859184@126.com>
2021-02-08 13:13:11 +0800

    obj serializer

---
+++ trpc-common/src/main/java/com/netty/trpc/serializer/Serializer.java
@@ -0,0 +1,13 @@
+package com.netty.trpc.serializer;
+
+/**
+ * @author DuanMingJun
+ * @version 1.0
+ * @date 2021-02-08 13:02
+ */
+public interface Serializer {
+
+   <T> byte[] serialize(T obj);
+
+   <T> T deserialize(byte[] bytes,Class<T> clazz);
+}
