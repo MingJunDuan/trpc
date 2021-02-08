fc8b8e1dbbcd2a3493857770ce1efd1337ca7338
duanmingjun <mjduan@xx.com>
2021-02-08 09:25:40 +0800

    加上依赖

---
+++ trpc-common/src/test/java/com/netty/trpc/LOGTest.java
@@ -0,0 +1,20 @@
+package com.netty.trpc;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author DuanMingJun
+ * @version 1.0
+ * @date 2021-02-08 9:23
+ */
+public class LOGTest {
+    private static final Logger log = LoggerFactory.getLogger(LOGTest.class);
+
+    @Test
+    public void test(){
+        LOG.info("Info test");
+        log.info("Info test");
+    }
+}
