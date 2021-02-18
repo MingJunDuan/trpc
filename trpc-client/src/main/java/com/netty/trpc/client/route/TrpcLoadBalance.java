59958f406e30170a117d1a3bcc627366b3e1af6b
duanmingjun <dmj1161859184@126.com>
2021-02-18 13:46:43 +0800

    Complete round robin router

--- trpc-client/src/main/java/com/netty/trpc/client/route/TrpcLoadBalance.java
+++ trpc-client/src/main/java/com/netty/trpc/client/route/TrpcLoadBalance.java
@@ -2,7 +2,12 @@ package com.netty.trpc.client.route;
 
 import com.netty.trpc.client.handler.TrpcClientHandler;
 import com.netty.trpc.common.protocol.RpcProtocol;
+import com.netty.trpc.common.protocol.RpcServiceInfo;
+import com.netty.trpc.common.util.ServiceUtil;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -11,5 +16,25 @@ import java.util.Map;
  * @date 2021-02-18 12:35
  */
 public abstract class TrpcLoadBalance {
-    public abstract RpcProtocol route(String serviceKey, Map<RpcProtocol, TrpcClientHandler> connectedServerNodes);
+
+    protected Map<String, List<RpcProtocol>> getServiceMap(Map<RpcProtocol,TrpcClientHandler> connectedServerNodes){
+        Map<String,List<RpcProtocol>> serviceMap=new HashMap<>();
+        if (connectedServerNodes!=null&&connectedServerNodes.size()>0){
+            for (RpcProtocol rpcProtocol : connectedServerNodes.keySet()) {
+                for (RpcServiceInfo rpcServiceInfo : rpcProtocol.getServiceInfoList()) {
+                    String serviceKey = ServiceUtil.serviceKey(rpcServiceInfo.getServiceName(), rpcServiceInfo.getVersion());
+                    List<RpcProtocol> rpcProtocolList = serviceMap.get(serviceKey);
+                    if (rpcProtocolList==null){
+                        rpcProtocolList=new LinkedList<>();
+                    }
+                    rpcProtocolList.add(rpcProtocol);
+                    serviceMap.putIfAbsent(serviceKey,rpcProtocolList);
+                }
+            }
+        }
+        return serviceMap;
+    }
+
+
+    public abstract RpcProtocol route(String serviceKey, Map<RpcProtocol, TrpcClientHandler> connectedServerNodes) throws Exception;
 }
--- trpc-client/src/main/java/com/netty/trpc/client/route/impl/TrpcLoadBalanceRoundRobin.java
+++ trpc-client/src/main/java/com/netty/trpc/client/route/impl/TrpcLoadBalanceRoundRobin.java
@@ -4,7 +4,9 @@ import com.netty.trpc.client.handler.TrpcClientHandler;
 import com.netty.trpc.client.route.TrpcLoadBalance;
 import com.netty.trpc.common.protocol.RpcProtocol;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author DuanMingJun
@@ -12,9 +14,22 @@ import java.util.Map;
  * @date 2021-02-18 12:36
  */
 public class TrpcLoadBalanceRoundRobin extends TrpcLoadBalance {
+    private AtomicInteger roundRobin = new AtomicInteger(0);
 
     @Override
-    public RpcProtocol route(String serviceKey, Map<RpcProtocol, TrpcClientHandler> connectedServerNodes) {
-        return null;
+    public RpcProtocol route(String serviceKey, Map<RpcProtocol, TrpcClientHandler> connectedServerNodes) throws Exception {
+        Map<String, List<RpcProtocol>> serviceMap = getServiceMap(connectedServerNodes);
+        List<RpcProtocol> addressList = serviceMap.get(serviceKey);
+        if (addressList!=null&&addressList.size()>0){
+            return doRoute(addressList);
+        }else {
+            throw new Exception("Can not find connection for service:"+serviceKey);
+        }
+    }
+
+    private RpcProtocol doRoute(List<RpcProtocol> addressList) {
+        int size = addressList.size();
+        int index = roundRobin.getAndAdd(1) % size;
+        return addressList.get(index);
     }
 }
