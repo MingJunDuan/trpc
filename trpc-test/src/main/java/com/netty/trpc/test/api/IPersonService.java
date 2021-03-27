a346028ddaaa5d6f36727e5cd9e69dac50a7cd08
duanmingjun <dmj1161859184@126.com>
2021-03-27 13:27:06 +0800

    调用服务

--- trpc-test/src/main/java/com/netty/trpc/test/api/IPersonService.java
+++ trpc-test/src/main/java/com/netty/trpc/test/api/IPersonService.java
@@ -5,5 +5,13 @@ import com.netty.trpc.test.service.Person;
 import java.util.List;
 
 public interface IPersonService {
+
+    /**
+     * 调用服务
+     *
+     * @param name
+     * @param num
+     * @return
+     */
     List<Person> callPerson(String name, Integer num);
 }
