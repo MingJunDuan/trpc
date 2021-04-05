5a02f53c00fb517c00dcdee8cd65e377163628a7
duanmingjun <dmj1161859184@126.com>
2021-04-05 23:00:40 +0800

    Fail back invoker

--- trpc-client/src/main/java/com/netty/trpc/client/faulttolerance/FailBackInvoker.java
+++ trpc-client/src/main/java/com/netty/trpc/client/faulttolerance/FailBackInvoker.java
@@ -1,11 +1,18 @@
 package com.netty.trpc.client.faulttolerance;
 
+import com.netty.trpc.common.codec.TrpcRequest;
+
 /**
  * @author DuanMingJun
  * @version 1.0
  * @date 2021-02-25 23:07
  */
-public class FailBackInvoker {
+public class FailBackInvoker implements Invoker{
+
+    @Override
+    public Object invoke(String serviceKey, TrpcRequest request) {
+        return null;
+    }
 
     //若调用成功则直接返回
     //若失败则保存上下文，而后定时任务去发送
