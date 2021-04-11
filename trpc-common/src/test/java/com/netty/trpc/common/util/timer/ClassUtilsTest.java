6821f61591de1f8a6ef9bd2974a3c56c218eaaef
MingJunDuan <dmj1161859184@126.com>
2021-04-11 21:50:44 +0800

    ClassUtils unit test

---
+++ trpc-common/src/test/java/com/netty/trpc/common/util/timer/ClassUtilsTest.java
@@ -0,0 +1,22 @@
+package com.netty.trpc.common.util.timer;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author DuanMingJun
+ * @version 1.0
+ * @date 2021-04-11 21:49
+ */
+public class ClassUtilsTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtilsTest.class);
+
+    @Test
+    public void simpleClassName() {
+        String className = ClassUtils.simpleClassName(ClassUtils.class);
+        LOGGER.info(className);
+    }
+}
