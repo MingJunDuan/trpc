c4463978787c59f6decd9af1d570584caa53596a
MingJunDuan <dmj1161859184@126.com>
2021-04-15 10:36:40 +0800

    HashedWheelTimer unit test

--- trpc-common/src/test/java/com/netty/trpc/common/util/timer/HashedWheelTimerTest.java
+++ trpc-common/src/test/java/com/netty/trpc/common/util/timer/HashedWheelTimerTest.java
@@ -1,6 +1,10 @@
 package com.netty.trpc.common.util.timer;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -10,10 +14,24 @@ import static org.junit.Assert.*;
  * @date 2021-04-12 09:03
  */
 public class HashedWheelTimerTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HashedWheelTimerTest.class);
 
     @Test
-    public void newTimeout() {
+    public void newTimeout() throws InterruptedException {
         HashedWheelTimer hashedWheelTimer = new HashedWheelTimer();
-        //hashedWheelTimer.newTimeout()
+        hashedWheelTimer.newTimeout(new TimerTask() {
+            @Override
+            public void run(Timeout timeout) throws Exception {
+                LOGGER.info("调用外部系统");
+            }
+        },4, TimeUnit.SECONDS);
+        LOGGER.info("start");
+        hashedWheelTimer.start();
+        LOGGER.info("start end");
+
+        TimeUnit.SECONDS.sleep(2);
+        hashedWheelTimer.stop();
+        LOGGER.info("stopped");
+        TimeUnit.SECONDS.sleep(4);
     }
 }
