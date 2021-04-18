6485cb3b126d66d30ac814afc958447cd0e49f08
MingJunDuan <dmj1161859184@126.com>
2021-04-18 18:49:45 +0800

    Broadcast invoker

--- trpc-client/src/main/java/com/netty/trpc/client/faulttolerance/BroadcastInvoker.java
+++ trpc-client/src/main/java/com/netty/trpc/client/faulttolerance/BroadcastInvoker.java
@@ -1,9 +1,16 @@
 package com.netty.trpc.client.faulttolerance;
 
+import com.netty.trpc.common.codec.TrpcRequest;
+
 /**
  * @author DuanMingJun
  * @version 1.0
  * @date 2021-04-01 14:55
  */
-public class BroadcastInvoker {
+public class BroadcastInvoker implements Invoker{
+
+    @Override
+    public Object invoke(String serviceKey, TrpcRequest request) {
+        return null;
+    }
 }
