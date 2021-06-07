ca4ba0518a41a00439e9149701cd3a739899008c
mjduan <dmj1161859184@126.com>
2021-06-07 23:41:53 +0800

    Format code and Add comment

--- trpc-client/src/main/java/com/netty/trpc/client/common/Holder.java
+++ trpc-client/src/main/java/com/netty/trpc/client/common/Holder.java
@@ -12,34 +12,34 @@ import java.util.Map;
  * @date 2021-06-07 23:25
  */
 public class Holder {
-    private static Map<Tuple2,Object> cache = new HashMap<>();
+    private static Map<Tuple2, Object> cache = new HashMap<>();
 
-    public static <T> T getServiceBean(Class<T> interfaceName,String version){
+    public static <T> T getServiceBean(Class<T> interfaceName, String version) {
         Object serviceBean = cache.get(new Tuple2(interfaceName, version));
-        if (serviceBean!=null){
+        if (serviceBean != null) {
             return (T) serviceBean;
         }
         return null;
     }
 
-    public static <T> void addServiceBean(Class<T> interfaceName,String version,Object bean){
+    public static <T> void addServiceBean(Class<T> interfaceName, String version, Object bean) {
         Tuple2 tuple2 = new Tuple2(interfaceName, version);
-        cache.put(tuple2,bean);
+        cache.put(tuple2, bean);
     }
 
     @Data
     @AllArgsConstructor
-    static class Tuple2{
+    static class Tuple2 {
         private Class interfaceNames;
         private String version;
 
         @Override
         public boolean equals(Object var1) {
-            if (var1==null || !(var1 instanceof Tuple2)){
+            if (var1 == null || !(var1 instanceof Tuple2)) {
                 return false;
             }
-            Tuple2 tuple2= (Tuple2) var1;
-            if (tuple2.getInterfaceNames()==interfaceNames && tuple2.getVersion().equals(version)){
+            Tuple2 tuple2 = (Tuple2) var1;
+            if (tuple2.getInterfaceNames() == interfaceNames && tuple2.getVersion().equals(version)) {
                 return true;
             }
             return false;
@@ -48,7 +48,8 @@ public class Holder {
         @Override
         public int hashCode() {
             int hashCode = this.interfaceNames.hashCode() ^ this.version.hashCode();
-            return hashCode ^ (hashCode>>>16);
+            //Inspired by JDK HashMap.hashCode()
+            return hashCode ^ (hashCode >>> 16);
         }
     }
 
