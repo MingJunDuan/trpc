0276142c24169174fad4b5dbcd3a8df304774885
MingJunDuan <dmj1161859184@126.com>
2021-11-23 00:32:05 +0800

    RegistryMetadata是注册中心的元数据

---
+++ trpc-registry-center/trpc-registry-center-common/src/main/java/com/netty/trpc/registrycenter/common/RegistryMetadata.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright(c): 2018 com.mjduan All rights reserved.
+ * 项目名：trpc
+ * 注意：未经作者允许，不得外传
+ */
+package com.netty.trpc.registrycenter.common;
+
+/**
+ * @author dmj1161859184@126.com 2021-11-23 00:31
+ * @version 1.0
+ * @since 1.0
+ */
+public class RegistryMetadata {
+
+}
