23d18d8636db12c65e5f8a6701801e0ffc2c85be
dmj1161859184@126.com <dmj1161859184@126.com>
2021-12-01 12:52:35 +0800

    zookeeper实现服务注册

--- trpc-registry-center/trpc-registry-center-provider/trpc-registry-center-provider-zookeeper/src/main/java/com/netty/trpc/registrycenter/provider/zookeeper/ZookeeperRegistryCenterRepository.java
+++ trpc-registry-center/trpc-registry-center-provider/trpc-registry-center-provider-zookeeper/src/main/java/com/netty/trpc/registrycenter/provider/zookeeper/ZookeeperRegistryCenterRepository.java
@@ -1,7 +1,17 @@
 package com.netty.trpc.registrycenter.provider.zookeeper;
 
+import com.alibaba.fastjson.JSONObject;
+import com.netty.trpc.common.constant.TrpcConstant;
+import com.netty.trpc.common.zookeeper.CuratorClient;
 import com.netty.trpc.registrycenter.common.RegistryMetadata;
 import com.netty.trpc.registrycenter.provider.api.RegistryCenterRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author mjduan
@@ -9,14 +19,30 @@ import com.netty.trpc.registrycenter.provider.api.RegistryCenterRepository;
  * @date 2021-12-01 12:31
  */
 public class ZookeeperRegistryCenterRepository implements RegistryCenterRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceRegistry.class);
+    private CuratorClient client;
+    private List<String> pathList = new LinkedList<>();
+
+    public ZookeeperRegistryCenterRepository(String registryAddress){
+        this.client = new CuratorClient(registryAddress);
+    }
 
     @Override
     public void registry(RegistryMetadata metadata) {
-
+        byte[] bytes = JSONObject.toJSONString(metadata).getBytes(StandardCharsets.UTF_8);
+        String path = TrpcConstant.ZK_DATA_PATH + "-" + metadata.hashCode();
+        try {
+            client.createPathData(path,bytes);
+            this.pathList.add(path);
+            LOGGER.info("Registry new service, host:{}, port:{}, interfaceName:{}",metadata.getHost(),metadata.getPort(),metadata.getServiceInfoList());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(),e);
+        }
     }
 
     @Override
     public void unregistry(RegistryMetadata metadata) {
-
+        //just close and zk will remove ephemeral data
+        client.close();
     }
 }
