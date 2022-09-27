66e410822e9eb5c6d37e666ee028777977647920
MingJunDuan <dmj1161859184@126.com>
2022-09-28 00:55:55 +0800

    [nacos registry] add nacos registry/unregistry

--- trpc-registry-center/trpc-registry-center-provider/trpc-registry-center-provider-nacos/src/main/java/com/netty/trpc/registrycenter/provider/nacos/NacosProviderRegistryCenterRepository.java
+++ trpc-registry-center/trpc-registry-center-provider/trpc-registry-center-provider-nacos/src/main/java/com/netty/trpc/registrycenter/provider/nacos/NacosProviderRegistryCenterRepository.java
@@ -1,8 +1,18 @@
 package com.netty.trpc.registrycenter.provider.nacos;
 
+import java.util.List;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingFactory;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import com.netty.trpc.registrycenter.common.RegistryCenterMetadata;
 import com.netty.trpc.registrycenter.common.RegistryMetadata;
+import com.netty.trpc.registrycenter.common.RpcServiceMetaInfo;
 import com.netty.trpc.registrycenter.provider.api.ProviderRegistryCenterRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author mjduan
@@ -10,19 +20,85 @@ import com.netty.trpc.registrycenter.provider.api.ProviderRegistryCenterReposito
  * @date 2021-12-01 12:31
  */
 public class NacosProviderRegistryCenterRepository implements ProviderRegistryCenterRepository {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NacosProviderRegistryCenterRepository.class);
+    static final String applicationName = "applicationName";
+    private static final String defaultServiceName = "defaultServiceName";
+    private static final String clusterName = "trpc-cluster";
+    private NamingService namingService;
 
     @Override
     public void init(RegistryCenterMetadata metadata) {
-
+        String nacosServerList = metadata.getServerList();
+        try {
+            namingService = NamingFactory.createNamingService(nacosServerList);
+        } catch (NacosException e) {
+            LOGGER.error("Init nacos naming server exception", e);
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void registry(RegistryMetadata metadata) {
+        CallbackHandle callbackHandle = new CallbackHandle() {
 
+            @Override
+            public void handle(Instance instance) {
+                try {
+                    namingService.registerInstance(instance.getServiceName(), instance);
+                } catch (NacosException e) {
+                    LOGGER.error("Registry instance exception", e);
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        doHandle(metadata, callbackHandle);
+    }
+
+    private void doHandle(RegistryMetadata metadata, CallbackHandle callbackHandle) {
+        int port = metadata.getPort();
+        String host = metadata.getHost();
+        String serviceName = getServiceName(metadata);
+        List<RpcServiceMetaInfo> serviceInfoList = metadata.getServiceInfoList();
+
+        Instance instance = new Instance();
+        instance.setIp(host);
+        instance.setPort(port);
+        instance.setHealthy(true);
+        instance.setWeight(100);
+        instance.setEphemeral(true);
+        instance.setServiceName(serviceName);
+        instance.setClusterName(clusterName);
+
+        callbackHandle.handle(instance);
     }
 
     @Override
     public void unregistry(RegistryMetadata metadata) {
+        CallbackHandle callbackHandle = new CallbackHandle() {
 
+            @Override
+            public void handle(Instance instance) {
+                try {
+                    namingService.deregisterInstance(instance.getServiceName(), instance);
+                } catch (NacosException e) {
+                    LOGGER.error("Registry instance exception", e);
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+        doHandle(metadata, callbackHandle);
+    }
+
+    private String getServiceName(RegistryMetadata metadata) {
+        if (metadata.getProperties() != null) {
+            return metadata.getProperties().getProperty(applicationName);
+        }
+        return defaultServiceName;
+    }
+
+    interface CallbackHandle {
+
+        void handle(Instance instance);
     }
 }
