3333caa50fff7cfc49d813553e98fb1b524a63ce
MingJunDuan <dmj1161859184@126.com>
2022-11-10 00:03:37 +0800

    extract hessian2 serialization as a module

---
+++ trpc-serialization/trpc-serialization-hessian2/src/test/java/com/netty/trpc/serialization/hessian2/hessian/domain/TestEnum.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright(c): 2018 com.mjduan All rights reserved.
+ * 项目名：trpc
+ * 注意：未经作者允许，不得外传
+ */
+package com.netty.trpc.serialization.hessian2.hessian.domain;
+
+/**
+ * @author dmj1161859184@126.com 2021-12-21 22:24
+ * @version 1.0
+ * @since 1.0
+ */
+public enum TestEnum {
+    TEST1,TEST2
+}
