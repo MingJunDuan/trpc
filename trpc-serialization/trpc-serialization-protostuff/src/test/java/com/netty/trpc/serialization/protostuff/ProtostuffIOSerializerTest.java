96d91d7dc2231fdbe22cd2a6cf239aecea348ef6
MingJunDuan <dmj1161859184@126.com>
2022-11-10 00:23:36 +0800

    extract jdk serialization as a module

---
+++ trpc-serialization/trpc-serialization-protostuff/src/test/java/com/netty/trpc/serialization/protostuff/ProtostuffIOSerializerTest.java
@@ -0,0 +1,18 @@
+package com.netty.trpc.serialization.protostuff;
+
+import com.netty.trpc.common.extension.ExtensionLoader;
+import com.netty.trpc.serialization.api.Serializer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProtostuffIOSerializerTest {
+
+    @Test
+    public void serialize() {
+        ExtensionLoader<Serializer> extensionLoader = new ExtensionLoader<>(Serializer.class);
+        Serializer protobuffer = extensionLoader.getExtension("protobuffer");
+        Assert.assertNotNull(protobuffer);
+    }
+}
