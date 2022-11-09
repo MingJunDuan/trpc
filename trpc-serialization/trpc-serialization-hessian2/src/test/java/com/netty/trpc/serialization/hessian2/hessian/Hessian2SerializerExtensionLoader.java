d46df9fa0c5361d25d72271a02795ad06278e95d
MingJunDuan <dmj1161859184@126.com>
2022-11-10 00:17:43 +0800

    extract jdk serialization as a module

---
+++ trpc-serialization/trpc-serialization-hessian2/src/test/java/com/netty/trpc/serialization/hessian2/hessian/Hessian2SerializerExtensionLoader.java
@@ -0,0 +1,23 @@
+package com.netty.trpc.serialization.hessian2.hessian;
+
+import com.netty.trpc.common.extension.ExtensionLoader;
+import com.netty.trpc.serialization.api.Serializer;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Hessian2SerializerExtensionLoader {
+
+    @Test
+    public void test2(){
+        ExtensionLoader<Serializer> extensionLoader = new ExtensionLoader<>(Serializer.class);
+        Serializer hessian2Serializer = extensionLoader.getExtension("hessian2");
+        Assert.assertNotNull(hessian2Serializer);
+    }
+
+    @Test
+    public void test(){
+        ExtensionLoader<Serializer> extensionLoader = new ExtensionLoader<>(Serializer.class);
+        Serializer hessian2Serializer = extensionLoader.getExtension("hessian");
+        Assert.assertNotNull(hessian2Serializer);
+    }
+}
--- trpc-serialization/trpc-serialization-jdk/src/main/java/com/netty/trpc/serialization/jdk/JDKNativeSerializer.java
+++ trpc-serialization/trpc-serialization-jdk/src/main/java/com/netty/trpc/serialization/jdk/JDKNativeSerializer.java
@@ -1,4 +1,20 @@
 package com.netty.trpc.serialization.jdk;
 
-public class JDKNativeSerializer {
+import com.netty.trpc.serialization.api.Serializer;
+
+public class JDKNativeSerializer implements Serializer {
+    @Override
+    public short type() {
+        return 0;
+    }
+
+    @Override
+    public <T> byte[] serialize(T t) {
+        return new byte[0];
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> aClass) {
+        return null;
+    }
 }
