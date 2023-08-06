b888961e6bd673aa82ec8813498e2ef33992e7b8
MingJunDuan <dmj1161859184@126.com>
2023-08-07 00:48:37 +0800

    [transport] transport model and plan to support grpc triple

---
+++ trpc-remoting/src/main/java/com/netty/trpc/remoting/Server.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright(c): 2018 com.mjduan All rights reserved.
+ * 项目名：trpc
+ * 注意：未经作者允许，不得外传
+ */
+package com.netty.trpc.remoting;
+
+/**
+ * @author dmj1161859184@126.com 2023-08-07 00:47
+ * @version 1.0
+ * @since 1.0
+ */
+public class Server {
+}
