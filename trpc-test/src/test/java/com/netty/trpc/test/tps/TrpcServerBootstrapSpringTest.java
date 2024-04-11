437950c9b033fc06373dba6b8273bd250a00dbcc
MingJunDuan <dmj1161859184@126.com>
2024-04-11 23:35:08 +0800

    Fix zk protocol issue

--- trpc-test/src/test/java/com/netty/trpc/test/tps/TrpcServerBootstrapSpringTest.java
+++ trpc-test/src/test/java/com/netty/trpc/test/tps/TrpcServerBootstrapSpringTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @version 1.0
  * @date 2021-02-18 14:41
  */
-public class TrpcServerBootstrapSpring {
+public class TrpcServerBootstrapSpringTest {
 
     public static void main(String[] args){
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("server-spring.xml");
